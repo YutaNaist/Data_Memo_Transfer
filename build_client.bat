1222022c47261d88e396cff8145e011e3591eabc
YutaNaist <yuta.yamamoto@ms.naist.jp>
2024-12-19 16:34:19 +0900

    change build bat file

--- build_client.bat
+++ build_client.bat
@@ -2,8 +2,14 @@
 setlocal enabledelayedexpansion
 @REM .\compileUiToPy.bat
 
-pyinstaller.exe Data_Memo_transfer.py -F
-@REM pyinstaller.exe Data_Memo_transfer.py -F -w
+REM Data_Memo_transfer.py -F
+pyinstaller.exe Data_Memo_transfer.py -F -w
 @REM pyinstaller.exe Data_Memo_transfer.py -F
 mkdir .\dist\icons
 copy /y .\icons\*.png .\dist\icons\
+mkdir .\dist\forms
+copy /y .\forms\*.ui .\dist\forms\
+mkdir .\dist\settings
+copy /y .\settings\*.json .\dist\settings\
+echo "Succeed to build"
+pause 60
