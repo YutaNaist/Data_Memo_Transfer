20ad9e2a63b4b4dad0f4687b6a449d83fcdd97af
YutaNaist <yuta.yamamoto@ms.naist.jp>
2025-03-07 11:06:22 +0900

    Add: update install script to include Miniconda installation and environment setup

--- install_pyinstaller.bat
+++ install_pyinstaller.bat
@@ -1,8 +1,37 @@
+@echo off
+echo "Start initialize for diamond system %0"
+pause
+if exist "%USERPROFILE%\miniconda3" (
+    echo "Skip install miniconda"
+) else (
+    echo "Install the miniconda latest version on %USERPROFILE%"
+    curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -o "%USERPROFILE%\miniconda.exe"
+    start /wait "" "%USERPROFILE%\miniconda.exe" /S
+    del "%USERPROFILE%\miniconda.exe"
+    "%USERPROFILE%\miniconda3\Scripts\conda.exe" init
+    echo "Finish install ing miniconda. restart this bat file again"
+    call %0
+    exit
+)
+
+"%USERPROFILE%\miniconda3\Scripts\conda.exe" init
+
+if exist "%USERPROFILE%\miniconda3\envs\diamond" (
+    echo "Skip create conda environment diamond: already exist"
+) else (
+    echo "Create environment for diamond from yaml file."
+    @REM "%USERPROFILE%\miniconda3\condabin\conda.bat" create -n diamond python=3.11 -y
+    "%USERPROFILE%\miniconda3\condabin\conda.bat" env create -f "%CD%\environment_diamond.yml"
+)
+
+
 REM To install with this script, MinGW and python with pip is required before running bat file.
 git clone https://github.com/pyinstaller/pyinstaller.git
 REM winget install MartinStorsjo.LLVM-MinGW.MSVCRT
 cd .\pyinstaller\bootloader\
 python .\waf distclean all
 cd ..
-pip install wheel
+@REM pip install wheel
 pip install .
+cd ..
+rm -rf .\pyinstaller
