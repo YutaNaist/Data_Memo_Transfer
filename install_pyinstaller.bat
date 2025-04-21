39457d7c8a36e00a13b64c18012ea688f209f340
YutaNaist <yuta.yamamoto@ms.naist.jp>
2025-04-21 15:54:35 +0900

    Fix: update messages for miniconda installation and environment creation in install_pyinstaller.bat

--- install_pyinstaller.bat
+++ install_pyinstaller.bat
@@ -2,7 +2,7 @@
 echo "Start initialize for diamond system %0"
 pause
 if exist "%USERPROFILE%\miniconda3" (
-    echo "Skip install miniconda"
+    "Skip install the miniconda latest version: already exist"
 ) else (
     echo "Install the miniconda latest version on %USERPROFILE%"
     curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -o "%USERPROFILE%\miniconda.exe"
@@ -17,12 +17,19 @@ if exist "%USERPROFILE%\miniconda3" (
 "%USERPROFILE%\miniconda3\Scripts\conda.exe" init
 
 if exist "%USERPROFILE%\miniconda3\envs\data-memo-transfer-PyQt5" (
-    echo "Skip create conda environment data-memo-transfer-PyQt5: already exist"
+    echo "Delete environment data-memo-transfer-PyQt5: already exist"
+    @REM "Skip create conda environment diamond: already exist"
+    "%USERPROFILE%\miniconda3\condabin\conda.bat" env remove -n data-memo-transfer-PyQt5 -y
+    echo "Create environment again for diamond from yaml file."
+    "%USERPROFILE%\miniconda3\condabin\conda.bat" env create -f "%CD%\environment_diamond.yml"
 ) else (
     echo "Create environment for data-memo-transfer-PyQt5 from yaml file."
     @REM "%USERPROFILE%\miniconda3\condabin\conda.bat" create -n diamond python=3.11 -y
     "%USERPROFILE%\miniconda3\condabin\conda.bat" env create -f "%CD%\environment_data-memo-transfer.yaml"
 )
+
+echo "Finish to install diamond system."
+echo "Next step: run ./run-diamond.bat"
 
 
 @REM REM To install with this script, MinGW and python with pip is required before running bat file.
