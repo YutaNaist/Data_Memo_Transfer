e79e303810bd91d13b1819298d51c38327932362
YutaNaist <yuta.yamamoto@ms.naist.jp>
2025-04-24 17:43:38 +0900

    Add install_build_env.bat for setting up Conda environment

---
+++ install_build_env.bat
@@ -0,0 +1,42 @@
+@echo off
+echo "Start initialize for diamond system %0"
+echo "-----Create Conda environment for diamond-----"
+
+if exist "%USERPROFILE%\miniconda3" (
+    echo "Skip install the miniconda latest version: already exist"
+) else (
+    echo "Install the miniconda latest version on %USERPROFILE%"
+    curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -o "%USERPROFILE%\miniconda.exe"
+    start /wait "" "%USERPROFILE%\miniconda.exe" /S
+    del "%USERPROFILE%\miniconda.exe"
+    call "%USERPROFILE%\miniconda3\Scripts\conda.exe" init
+    echo "Finish install ing miniconda. restart this bat file again"
+    call %0
+    exit
+)
+
+call "%USERPROFILE%\miniconda3\Scripts\conda.exe" init
+
+if exist "%USERPROFILE%\miniconda3\envs\data-memo-transfer-PyQt5" (
+    echo "Delete environment data-memo-transfer-PyQt5: already exist"
+    call "%USERPROFILE%\miniconda3\condabin\conda.bat" env remove -n data-memo-transfer-PyQt5 -y
+    echo "Create environment again for diamond from yaml file."
+    call "%USERPROFILE%\miniconda3\condabin\conda.bat" env create -f "%CD%\environment_data-memo-transfer.yaml"
+) else (
+    echo "Create environment for data-memo-transfer-PyQt5 from yaml file."
+    call "%USERPROFILE%\miniconda3\condabin\conda.bat" env create -f "%CD%\environment_data-memo-transfer.yaml"
+)
+
+@REM REM To install with this script, MinGW and python with pip is required before running bat file.
+@REM git clone https://github.com/pyinstaller/pyinstaller.git
+@REM winget install MartinStorsjo.LLVM-MinGW.MSVCRT
+@REM cd .\pyinstaller\bootloader\
+@REM python .\waf distclean all
+@REM cd ..
+@REM @REM pip install wheel
+@REM pip install .
+@REM cd ..
+@REM rm -rf .\pyinstaller
+
+echo "Finish installing environment for data-memo-transfer-PyQt5."
+echo "Next step: run build_client.bat to build the client."
